53bfe235d71f6db72fc203286880dd3dc02bd569
Magda <m.przybyla@warwick.ac.uk>
2019-07-29 19:58:31 +0100

    Solved

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,2 +1,6 @@
-def two_fer(name):
-    pass
+def two_fer(name = None):
+    if name == None:
+        return 'One for you, one for me.'
+    else:
+        return  'One for ' + name + ', one for me.' 
+
