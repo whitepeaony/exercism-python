397b0f541a37f9e6853717210ca9c8d636113326
Magda <m.przybyla@warwick.ac.uk>
2019-07-30 17:47:40 +0100

    Solved

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,9 @@
 def distance(strand_a, strand_b):
-    pass
+    count = 0
+    if len(strand_a) != len(strand_b):
+        raise ValueError('Strands should have the same length')
+    
+    for a, b in zip(strand_a, strand_b):
+        if a != b:
+            count += 1 
+    return count 
