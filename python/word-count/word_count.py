cec1ede8e82322df66bb0e2a5c06323380356cd1
Magda <m.przybyla@warwick.ac.uk>
2019-08-01 22:08:40 +0100

    Solved

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,14 @@
 def count_words(sentence):
-    pass
+    sentence = sentence.lower()
+    words = {}
+    shit = ',\n:!&@$%^&._'
+    for s in shit:
+        sentence = sentence.replace(s, ' ')
+    for w in sentence.split():
+        if w.endswith('\''):
+            w = w[:-1]
+        if w.startswith('\''):
+            w = w[1:]
+        words[w] = words.get(w, 0) + 1
+    return words
+
