fd362c5acb12e3cb56d2e23ad458dd1dbb9777cc
Hubert Bereś <h.beres@hotmail.com>
2019-08-02 15:59:55 +0100

    Logic can be made conceise with boolean operators

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -3,7 +3,4 @@ def is_isogram(string):
     white_string = string.replace(' ', '').replace('-', '').lower()
     b = [white_string.count(char) for char in white_string]
 
-    if len(b) < 2:
-        return True
-    else:
-        return max(b) <= 1
+    return len(b) < 2 or max(b) <= 1
