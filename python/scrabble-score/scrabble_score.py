0a6057df657242707fc0ca2fc238c48d784d0112
Magda <m.przybyla@warwick.ac.uk>
2019-08-02 10:59:52 +0100

    Solved

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,2 +1,24 @@
 def score(word):
-    pass
+
+    word = word.lower()
+    letters = {}
+
+    scrabble = [
+        ['a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'],
+        ['d', 'g'],
+        ['b', 'c', 'm', 'p'],
+        ['f', 'h', 'v', 'w', 'y'],
+        ['k'],
+        ['j', 'x'],
+        ['q', 'z']
+    ]
+    scores = [1, 2, 3, 4, 5, 8, 10]
+
+    total = 0
+    for l in word:
+        for i, s in zip(scrabble, scores):
+            if l in i:
+                letters[l] = s
+        total += letters.get(l, 0)
+
+    return total
