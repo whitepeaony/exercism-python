407636745a402e6fbd54004c78299fa7153985d2
Magda <m.przybyla@warwick.ac.uk>
2019-08-03 19:12:40 +0100

    Variable inlined

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,14 +1,13 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        lines = matrix_string.split('\n')
-        a = []
-        for i in lines:
+        self.matrix = []
+        for i in matrix_string.split('\n'):
             b = [int(l) for l in i.split()]
-            a.append(b)
-        self.matrix = a:
+            self.matrix.append(b)
+       
 
     def row(self, index):
-        return self.matrix[index-1]
+        return self.matrix[index - 1]
 
     def column(self, index):
-        return [ r[index-1] for r in self.matrix]
+        return [ r[index - 1] for r in self.matrix]
