29b5573080c1a94d458bb188ac80a36989d7ec48
Magda <m.przybyla@warwick.ac.uk>
2019-08-03 13:28:29 +0100

    Solved

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,27 @@
 class Garden(object):
-    def __init__(self, diagram, students):
-        pass
+    def __init__(self, diagram, students=None):
+
+        if students is None:
+            students = ['Alice',  'Bob',  'Charlie',  'David', 'Eve',  'Fred',
+                        'Ginny',  'Harriet',  'Ileana',  'Joseph',  'Kincaid',  'Larry']
+        
+        cups = []
+        a, b = diagram.split('\n')
+       
+        for i in range(0, len(a), 2):
+           cups.append(a[i:i+2] + b[i:i+2])
+           
+
+        self.seeds = {}
+        for n, i in zip(sorted(students), cups):
+            self.seeds[n] = i
+
+    full_names = {'C':'Clover', 'R':'Radishes', 'G':'Grass', 'V':'Violets'}
+
+
+    def plants(self, name):
+        seed = self.seeds[name]
+        return  [self.full_names[s] for s in seed] 
+
+        
+
