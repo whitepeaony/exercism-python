bf657f57d935741b9788da58f4b55e980d2271c8
Magda <m.przybyla@warwick.ac.uk>
2019-08-06 19:35:18 +0100

    Polished

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,32 +1,22 @@
 class Luhn(object):
     def __init__(self, card_num):
         self.card_num = card_num.replace(' ', '')
-              
-
-        
 
     def valid(self):
         if not self.card_num.isdigit():
             return False
 
-        card_num = [ch for ch in self.card_num]
-
-        if len(card_num) <= 1:
+        if len(self.card_num) <= 1:
             return False
-       
-        card = [int(i) for i in card_num]
+
+        card = [int(ch) for ch in self.card_num]
 
         for i in range(len(card)-2, -1,  -2):
-            
+
             if card[i] * 2 < 9:
                 card[i] *= 2
-            
+
             else:
-                card[i] =  2 *card[i] - 9
-            
-        return sum(card) % 10 ==0
-            
+                card[i] = 2 * card[i] - 9
 
-
-#print(Luhn("055 444 285").valid())
-
+        return sum(card) % 10 == 0
