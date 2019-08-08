dce9416f37697fabadd1d84e91a585aff514f07c
Magda <m.przybyla@warwick.ac.uk>
2019-08-08 22:08:55 +0100

    __eq__ (self, other)

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,6 +1,6 @@
 def _time(hour, minute):
     total = 60 * hour + minute
-    return [(total // 60) % 24, total % 60]
+    return (total // 60) % 24, total % 60
 
 class Clock(object):
     def __init__(self, hour, minute):
@@ -14,7 +14,10 @@ class Clock(object):
 
 
     def __eq__(self, other):
-        pass
+        s = _time(self.h, self.m)
+        o = _time(other.h, other.m)
+
+        return s[0] == o[0] and s[1] == o[1]
 
     def __add__(self, minutes):
         pass
