2c8a260559567c091082b53c3bf40a5305be8f0e
Magda <m.przybyla@warwick.ac.uk>
2019-08-08 21:34:26 +0100

    Not smart zero-padding

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,9 +1,30 @@
+def _time(hour, minute):
+    total = 60 * hour + minute
+    hours = total // 60
+    minut = total % 60
+    return [hours, minut]
+
 class Clock(object):
     def __init__(self, hour, minute):
-        pass
+        self.h = hour
+        self.m = minute
 
+    
     def __repr__(self):
-        pass
+        t = _time(self.h, self.m)
+        if t[0] < 10:
+            a = 0
+        else:
+            a =''
+        if t[1] < 10:
+            b = 0
+        else:
+            b =''
+
+        return "{}{}:{}{}".format(a, t[0], b, t[1])
+
+ 
+
 
     def __eq__(self, other):
         pass
@@ -13,3 +34,6 @@ class Clock(object):
 
     def __sub__(self, minutes):
         pass
+
+print(Clock(2, 20))
+
