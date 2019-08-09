a70959b15c5e9e5929256b639cef63280f396ec2
Magda <m.przybyla@warwick.ac.uk>
2019-08-09 21:23:06 +0100

    bold and italic
    backreference, sub (replace)

--- python/markdown/crap.py
+++ python/markdown/crap.py
@@ -5,20 +5,19 @@ def _headers(line: str) -> str:
     m =re.match('(?P<level>#+) +(?P<header>.*)', line)
     if m:
         n = len(m.group('level'))
-        return "<h{}> {} </h{}>".format(n, m.group('header'), n)
+        return "<h{}>{}</h{}>".format(n, m.group('header'), n)
     else: 
         return line
 
 def _boldness(line: str) -> str:
-    bold = re.compile('__')
-    a = bold.sub('<strong>', _headers(line), count=1 )
-    return bold.sub('</strong>', a, count=1)
+    return re.sub('__(.+?)__', '<strong>\1</strong>', line)
 
 def _italicisation(line: str) -> str:
-    italics = re.compile('_')
-    a = italics.sub('<em>', _boldness(line), count=1 )
-    return italics.sub('</em>', a, count=1)
-
+    '''
+    Run after _boldness, to avoid confusiuon with double underline
+    '''
+    return re.sub('_(.+?)_', '<em>\1</em>', line)
+   
 def _bullet(line: str, inlist: bool ) -> (str, bool):
     m = re.compile(r'*')
     n = re.compile(r'\n*')
@@ -33,6 +32,8 @@ def _bullet(line: str, inlist: bool ) -> (str, bool):
 
 def parse(markdown: str) -> str:
     lines = markdown.split('\n')
-    
-    pass
+    result = ''
+    for l in lines:
+        result += _headers(l)
+    return result
     
